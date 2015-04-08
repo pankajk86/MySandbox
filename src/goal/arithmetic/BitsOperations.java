d389dc98c2997724cc7a73f78acea460df017775
Pankaj Kumar <pankajkumar@Pankajs-MacBook-Pro.local>
2015-04-07 22:03:22 -0500

    added rightbit manipulation java file

--- src/goal/arithmetic/BitsOperations.java
+++ src/goal/arithmetic/BitsOperations.java
@@ -3,8 +3,7 @@ package goal.arithmetic;
 public class BitsOperations {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		RightBitManipulation.turnOffRightOneBit("01011001");
 	}
 
 }
---
+++ src/goal/arithmetic/RightBitManipulation.java
@@ -0,0 +1,28 @@
+package goal.arithmetic;
+
+public class RightBitManipulation {
+	
+	/**
+	 * To turn off the rightmost 1-bit in a word (passed as a {@code String}).
+	 * <br/>
+	 * 
+	 * btoi & (btoi - 1), where
+	 * 
+	 * btoi = Integer.parseInt(word, 2)
+	 * 
+	 * @param bString word string
+	 */
+	public static void turnOffRightOneBit(String bString){
+		int btoi = Integer.parseInt(bString, 2);
+		System.out.println("Binary: " + bString + ", integer: " + btoi);
+		int number = btoi & (btoi - 1);
+		System.out.println("Binary: " + Integer.toBinaryString(number) + ", integer: " +  number);
+		int number2 = number & (number - 1);
+		System.out.println("Binary: " + Integer.toBinaryString(number2) + ", integer: " +  number2);
+		int number3 = number2 & (number2 - 1);
+		System.out.println("Binary: " + Integer.toBinaryString(number3) + ", integer: " +  number3);
+		int number4 = number3 & (number3 - 1);
+		System.out.println("Binary: " + Integer.toBinaryString(number4) + ", integer: " +  number4);
+	}
+
+}
