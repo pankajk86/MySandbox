5834a38670dde7dea1a67e5ab89c3893983c414a
pankajk86 <kumar.pankaj.it@gmail.com>
2015-11-27 18:51:24 -0600

    Created a generics interface TypeConverter

---
+++ src/goal/java/generics/StringIntegerConverter.java
@@ -0,0 +1,15 @@
+package goal.java.generics;
+
+public class StringIntegerConverter implements TypeConverter<String, Integer>{
+
+	@Override
+	public Integer convert(String string) {
+		return Integer.parseInt(string);
+	}
+
+	@Override
+	public String restore(Integer integer) {
+		return integer.toString();
+	}
+
+}
