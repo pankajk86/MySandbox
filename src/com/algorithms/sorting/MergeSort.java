ec51a4460d40f90ad0f82f3973c9830cebc31eb9
pankajk86 <kumar.pankaj.it@gmail.com>
2016-02-03 22:19:50 -0600

    added comments

--- src/com/algorithms/sorting/MergeSort.java
+++ src/com/algorithms/sorting/MergeSort.java
@@ -16,7 +16,7 @@ public class MergeSort {
 			int mid = (left + right)/2;
 			mergeSort(A, left, mid);
 			mergeSort(A, mid+1, right);
-			merge(A, left, mid+1, right);
+			merge(A, left, mid+1, right); // why did I do mid+1 here? A: that is the beginning of second part
 		}
 	}
 
@@ -48,7 +48,18 @@ public class MergeSort {
 			j++;
 		}
 		
-		for(int i=0; i< num; i++) {
+		// how this for loop works? 
+		/*
+		 * A: The idea is to move everything from temp to A array.
+		 * now, we can't rely on the length of the array A, because at a time we
+		 * might be working on a small part of that. So that's why, we are 
+		 * using (right-left+1) to calculate the actual number of elements under current split.
+		 * 
+		 * The reason, why we are using "right" is because that's the only variable 
+		 * (out of all left, mid and right) which has been left untouched until now.
+		 * So, we use it to move elements from temp to A.
+		 */
+		for(int i=0; i< num; i++) { 
 			A[right] = temp[right];
 			right--;
 		}
