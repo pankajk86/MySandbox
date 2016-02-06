f156aee13275a4efbee84e61cad8cf5c9e4303bb
pankajk86 <kumar.pankaj.it@gmail.com>
2016-02-06 17:25:51 -0600

    moved Mergesort

--- src/goal/algorithms/dnc/MergeSort.java
+++ src/goal/algorithms/dnc/MergeSort.java
@@ -1,4 +1,4 @@
-package com.algorithms.sorting;
+package goal.algorithms.dnc;
 
 public class MergeSort {
 
---
+++ src/goal/algorithms/dnc/Quicksort.java
@@ -0,0 +1,10 @@
+package goal.algorithms.dnc;
+
+public class Quicksort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
