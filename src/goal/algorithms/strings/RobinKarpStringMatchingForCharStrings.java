88624fedae936e98cdb6bfc55a0633e15946f3a7
pankajk86 <kumar.pankaj.it@gmail.com>
2016-02-14 18:42:02 -0600

    Added Robin-karp

---
+++ src/goal/algorithms/strings/RobinKarpStringMatchingForCharStrings.java
@@ -0,0 +1,56 @@
+package goal.algorithms.strings;
+
+public class RobinKarpStringMatchingForCharStrings {
+
+	public static void main(String[] args) {
+		String text = "abcdefgh";
+		String pattern = "abcd";
+		
+		int startLoc = searchPattern(text, pattern);
+		System.out.println("Starting Location of the pattern in text: " + startLoc);
+	}
+
+	private static int searchPattern(String text, String pattern) {
+		
+		int patternHash = 1;
+		int n = text.length();
+		int m = pattern.length();
+		
+		for(int i=0; i< m; i++) {
+			char c = pattern.charAt(i);
+			int primeCode = Characters.valueOf(String.valueOf(c)).getPrimeCode();
+			patternHash *= primeCode;
+		}
+		
+		for(int i=0; i< (n-m+1); i++) {
+			int textPartHash = 1;
+			
+			for(int j=i; j<(i+m); j++) {
+				char c = text.charAt(j);
+				int primeCode = Characters.valueOf(String.valueOf(c)).getPrimeCode();
+				textPartHash *= primeCode;
+			}
+			
+			if(textPartHash == patternHash) {
+				return i;
+			}
+		}
+			
+		return -1;
+	}
+}
+
+enum Characters {
+
+	a(2), b(3), c(5), d(7), e(9), f(11), g(13), h(17); 
+
+	private final int primeCode;
+
+	Characters(int primeCode) {
+        this.primeCode = primeCode;
+    }
+
+	public int getPrimeCode() {
+        return this.primeCode;
+    }
+}
