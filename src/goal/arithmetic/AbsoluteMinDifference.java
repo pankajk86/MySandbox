1406d90c35587513b3f21c823c377701916bf9c2
pankajk86 <kumar.pankaj.it@gmail.com>
2016-02-20 10:11:01 -0600

    updated AbsoluteMinDifference.java

--- src/goal/arithmetic/AbsoluteMinDifference.java
+++ src/goal/arithmetic/AbsoluteMinDifference.java
@@ -2,17 +2,15 @@ package goal.arithmetic;
 
 import goal.algorithms.dnc.MergeSort;
 
-public class AbsoluteMinMaxDifference {
+public class AbsoluteMinDifference {
 
 	public static void main(String[] args) {
-		int arr[] = {-4, 2, 3, 9, -200, 0, 7, 17, 91,};
+		int arr[] = {-4, 3, 9, -200, 0, 7, 17, 91,};
 		
 		MergeSort.mergeSort(arr, 0, arr.length-1);
 		int minDiff = getMinAbsDifference(arr);
-		int maxDiff = getMaxAbsDifference(arr);
 		
 		System.out.println("Minimum Absolute Difference: " + minDiff);
-		System.out.println("Maximum Absolute Difference: " + maxDiff);
 	}
 
 	private static int getMinAbsDifference(int arr[]) {
@@ -26,18 +24,5 @@ public class AbsoluteMinMaxDifference {
 		}
 		
 		return minDiff;
-	}
-	
-	private static int getMaxAbsDifference(int arr[]) {
-		int maxDiff = Integer.MIN_VALUE;
-		
-		for(int i=0; i<(arr.length-1); i++) {
-			int val = arr[i+1]-arr[i];
-			if(val > maxDiff) {
-				maxDiff = val;
-			}
-		}
-		
-		return maxDiff;
 	}
 }
