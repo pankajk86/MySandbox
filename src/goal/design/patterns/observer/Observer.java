ad1d7f1d9c8ef3517de7058e383d0aa69a6a6c98
pankajk86 <kumar.pankaj.it@gmail.com>
2016-02-21 22:02:32 -0600

    added single threaded ObserverPattern

---
+++ src/goal/design/patterns/observer/Observer.java
@@ -0,0 +1,6 @@
+package goal.design.patterns.observer;
+
+public interface Observer {
+
+	public void update(double ibmPrice, double applPrice, double googPrice);
+}
---
+++ src/goal/design/patterns/observer/Subject.java
@@ -0,0 +1,8 @@
+package goal.design.patterns.observer;
+
+public interface Subject {
+
+	public void register(Observer o);
+	public void unregister(Observer o);
+	public void notifyObservers();
+}
