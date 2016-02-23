da7a4419adc22cbf555ea10e3ed855cb413b5c0b
pankajk86 <kumar.pankaj.it@gmail.com>
2016-02-22 22:34:04 -0600

    FirstIndexOfMaxOccurances.java

---
+++ src/goal/design/patterns/singleton/Singleton.java
@@ -0,0 +1,26 @@
+package goal.design.patterns.singleton;
+
+public class Singleton {
+	private static Singleton instance = null;
+	
+	private Singleton(){}
+	
+	/**
+	 * If not already created, then only it will create a new instance.
+	 * 
+	 * @return Singleton instance
+	 */
+	public Singleton getInstance() {
+		
+		if(instance == null) {
+			synchronized(Singleton.class) {
+				if(instance == null) {
+					// lazy instantiation
+					instance = new Singleton();
+				}
+			}
+		}
+		
+		return instance;
+	}
+}
