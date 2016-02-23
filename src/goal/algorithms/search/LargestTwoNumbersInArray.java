4a9fd9d93aaf32a4be766e10f4752db0b24be677
pankajk86 <kumar.pankaj.it@gmail.com>
2016-02-22 21:18:31 -0600

    added LargestTwoNumbersInArray.java

---
+++ src/goal/algorithms/search/LargestTwoNumbersInArray.java
@@ -0,0 +1,33 @@
+package goal.algorithms.search;
+
+public class LargestTwoNumbersInArray {
+
+	public static void main(String[] args) {
+		int[] arr = {3,6,4,1,5,2,10,20,30,25,29};
+		int largestTwo[] = getLargestTwoNumbers(arr);
+		
+		System.out.println(largestTwo[0] + ", " + largestTwo[1]);
+	}
+
+	private static int[] getLargestTwoNumbers(int[] arr) {
+		// l1 will contain the largest, l2 will contain the second largest
+		int l1 = arr[0], l2 = arr[0];
+		
+		for(int val: arr) {
+			// if next "val" is largest than l1, we will update both l1 and l2.
+			if(val > l1) {
+				l2 = l1;
+				l1 = val;
+			} 
+			// but if next "val" is greater than only l2, we will update only l2.
+			else if(val > l2 && val < l1) {
+				l2 = val;
+			}
+		}
+		
+		int newArr[] = {l1, l2};
+		
+		return newArr;
+	}
+
+}
---
+++ src/goal/java/threading/RoundRobinMultiThreading.java
@@ -0,0 +1,58 @@
+package goal.java.threading;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Needs re-work. It's NOT correct.
+ */
+public class RoundRobinMultiThreading {
+
+	public static void main(String[] args) {
+		Thread t1 = new Thread(new RoundRobinLogic());
+		Thread t2 = new Thread(new RoundRobinLogic());
+		Thread t3 = new Thread(new RoundRobinLogic());
+		
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+
+}
+
+class RoundRobinLogic implements Runnable
+{
+    private static final AtomicInteger systemIndex = new AtomicInteger(1);
+
+    private static final RemoteSystem[] systems = new RemoteSystem[] {
+        new RemoteSystem("System1"),
+        new RemoteSystem("System2"),
+        new RemoteSystem("System3"),
+        new RemoteSystem("System4"),
+    };
+
+    public static final Queue<String> output = new ConcurrentLinkedDeque<>();
+
+    @Override
+    public void run()
+    {
+        RemoteSystem system = systems[systemIndex.incrementAndGet() % systems.length];
+
+        // Sending the request to right system
+        output.add("Request to " + system.name());
+    }
+}
+
+class RemoteSystem
+{
+    private final String name;
+
+    RemoteSystem(String name) {
+        this.name = name;
+    }
+
+    public String name() {
+        return name;
+    }
+}
