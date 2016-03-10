0c7a23cb78ea049c82c95d987ae664c51df963bb
pankajk86 <kumar.pankaj.it@gmail.com>
2016-03-09 22:39:04 -0600

    added MaxSubArray.java

---
+++ src/goal/arithmetic/MaxSubArray.java
@@ -0,0 +1,33 @@
+package goal.arithmetic;
+
+public class MaxSubArray {
+
+	public static void main(String[] args) {
+
+		int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+		int[] maxSum = getMaxSubArraySum(arr);
+		int sum = 0, i=maxSum[0];
+		
+		while(sum != maxSum[1]) {
+			sum += arr[i];
+			i--;
+		}
+		
+		System.out.println("Begin Index: " + (i+1) + ", End Index: " + maxSum[0] + ", Max Sum: " + maxSum[1]);
+	}
+
+	private static int[] getMaxSubArraySum(int[] arr) {
+		int maxEndingHere = 0, maxSoFar = 0, maxEndIndex = 0;
+		
+		for(int i=0; i<arr.length; i++) {
+			maxEndingHere = Math.max(0, maxEndingHere + arr[i]);
+			if(maxEndingHere > maxSoFar) {
+				maxEndIndex = i;
+				maxSoFar = maxEndingHere;
+			}
+		}
+		
+		return new int[] {maxEndIndex, maxSoFar};
+	}
+
+}
