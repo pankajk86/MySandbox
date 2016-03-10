07340992d66e3d11c93a0601cdbc64094ebb7dba
pankajk86 <kumar.pankaj.it@gmail.com>
2016-03-09 23:23:07 -0600

    added SmallestElementGreaterThanTheKey.java

--- src/goal/arithmetic/MaxSubArray.java
+++ src/goal/arithmetic/MaxSubArray.java
@@ -4,7 +4,7 @@ public class MaxSubArray {
 
 	public static void main(String[] args) {
 
-		int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+		int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4, 9};
 		int[] maxSum = getMaxSubArraySum(arr);
 		int sum = 0, i=maxSum[0];
 		
---
+++ src/goal/arithmetic/SmallestElementGreaterThanTheKey.java
@@ -0,0 +1,32 @@
+package goal.arithmetic;
+
+public class SmallestElementGreaterThanTheKey {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1, 2, 3, 4, 5, 6, 8, 9, 13, 15, 16};
+		int key = 14;
+		
+		int result = getSmallestElementGreaterThanTheKey(arr, 0, arr.length-1, key);
+		System.out.println(result);
+	}
+
+	private static int getSmallestElementGreaterThanTheKey(int[] arr, int begin, int end, int key) {
+		
+		while(begin < end) {
+			int mid = begin + (end-begin)/2;
+			
+			if(arr[mid] > key && arr[mid-1] < key) {
+				return arr[mid];
+			}
+			
+			if(key < arr[mid]) {
+				end = mid;
+			} else {
+				begin = mid+1;
+			}
+		}
+		return 0;
+	}
+
+}
