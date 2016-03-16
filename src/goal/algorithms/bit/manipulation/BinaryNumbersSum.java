7f7dd19ba0a9d3a8843ae332b3932a36588d5ae0
pankajk86 <kumar.pankaj.it@gmail.com>
2016-03-15 23:45:57 -0500

    working on bit manipulation

---
+++ src/goal/algorithms/bit/manipulation/BinaryNumbersSum.java
@@ -0,0 +1,10 @@
+package goal.algorithms.bit.manipulation;
+
+public class BinaryNumbersSum {
+
+	// NEEDS TO BE IMPLEMENTED
+	public static void main(String[] args) {
+		String b1 = "1010", b2 = "1011";
+	}
+
+}
---
+++ src/goal/algorithms/bit/manipulation/CountSetBits.java
@@ -0,0 +1,18 @@
+package goal.algorithms.bit.manipulation;
+
+public class CountSetBits {
+
+	public static void main(String[] args) {
+
+		int n = 109; // 1101101
+		int count = 0;
+		
+		while(n != 0) {
+			n = n & (n-1);
+			count++;
+		}
+		
+		System.out.println(count);
+	}
+
+}
