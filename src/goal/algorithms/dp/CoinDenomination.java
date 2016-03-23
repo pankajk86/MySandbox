9d65ac3ba86965e5ab1ee0b47b1dcc774f02be24
pankajk86 <kumar.pankaj.it@gmail.com>
2016-03-23 17:47:46 -0400

    refactored coin denomination

--- src/goal/algorithms/dp/CoinDenomination.java
+++ src/goal/algorithms/dp/CoinDenomination.java
@@ -5,23 +5,19 @@ public class CoinDenomination {
 	public static void main(String[] args) {
 
 		int coins[] = { 1, 3, 5 };
-		int target = 44;
+		int target = 11;
 
-		// long start = System.currentTimeMillis();
 		// int minCoinDenomination = getMinCoinDenomination(coins, target);
-		// long end = System.currentTimeMillis();
-		// System.out.println("Minimum # of coins: " + minCoinDenomination);
-		// System.out.println("Time taken (in ms): " + (end-start));
 
 		long start = System.currentTimeMillis();
-		int minCoinDenomination = getMinCoinDenominationWithoutRecursion(coins, target);
+		int minCoinDenomination = getMinCoinDenominationWithoutRecursion(coins,
+				target);
 		long end = System.currentTimeMillis();
 		System.out.println("Minimum # of coins: " + minCoinDenomination);
 		System.out.println("Time taken (in ms): " + (end - start));
 	}
-	
-	private static int getMinCoinDenominationWithoutRecursion(int[] coins,
-			int target) {
+
+	private static int getMinCoinDenominationWithoutRecursion(int[] coins, int target) {
 		int coinsCache[] = new int[target + 1];
 
 		for (int i = 0; i <= target; i++) {
@@ -29,10 +25,10 @@ public class CoinDenomination {
 		}
 
 		coinsCache[0] = 0;
+		
 		for (int i = 1; i <= target; i++) {
 			for (int coin : coins) {
-				if (i >= coin && (i - coin) >= 0
-						&& coinsCache[i - coin] + 1 < coinsCache[i]) {
+				if ((i - coin) >= 0 && coinsCache[i - coin] + 1 < coinsCache[i]) {
 					coinsCache[i] = coinsCache[i - coin] + 1;
 				}
 			}
@@ -71,7 +67,5 @@ public class CoinDenomination {
 
 		return result;
 	}
-
-	
 
 }
