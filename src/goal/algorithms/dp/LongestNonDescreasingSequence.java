f253bfadcb670ec8ff257dfd23c9bae31e52438a
pankajk86 <kumar.pankaj.it@gmail.com>
2016-03-24 12:47:42 -0400

    added LongestNonDecreasingSequence.java

---
+++ src/goal/algorithms/dp/LongestNonDescreasingSequence.java
@@ -0,0 +1,33 @@
+package goal.algorithms.dp;
+
+public class LongestNonDescreasingSequence {
+
+	public static void main(String[] args) {
+
+		int arr[] = {1, 0, 3, 5, 3, 4, 8, 6, 5};
+		
+		int result = getLongestNonDescreasingSequence(arr);
+		System.out.println("# of longest non-decreasing sequence: " + result);
+	}
+
+	private static int getLongestNonDescreasingSequence(int[] arr) {
+		
+		int size = arr.length;
+		int[] result = new int[size];
+		
+		for(int i=0; i<size; i++) {
+			result[i] = 1;
+		}
+		
+		for(int i=1; i<size; i++) {
+			for(int j=0; j<i; j++) {
+				if(arr[j] <= arr[i] && result[j] + 1 > result[i] ) {
+					result[i] = result[j] + 1;
+				}
+			}
+		}
+		
+		return result[size-1];
+	}
+
+}
