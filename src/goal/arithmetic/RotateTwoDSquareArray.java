3001c024f10e9261123ace2e2c242c3daa5d69fa
pankajk86 <kumar.pankaj.it@gmail.com>
2016-04-02 18:47:20 -0500

    added two d arrays problems

---
+++ src/goal/arithmetic/RotateTwoDSquareArray.java
@@ -0,0 +1,33 @@
+package goal.arithmetic;
+
+public class RotateTwoDSquareArray {
+
+    public static void main(String[] args) {
+
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int n = arr.length;
+
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < Math.ceil(((double) n) / 2); j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[n - 1 - j][i];
+                arr[n - 1 - j][i] = arr[n - 1 - i][n - 1 - j];
+                arr[n - 1 - i][n - 1 - j] = arr[j][n - 1 - i];
+                arr[j][n - 1 - i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
