197fa0b08ead4745f501c160d55655920849113a
pankajk86 <kumar.pankaj.it@gmail.com>
2016-07-12 08:17:25 -0500

    phase2: working on LRUCache

---
+++ src/goal/design/system/lrucache/LRUCache.java
@@ -0,0 +1,89 @@
+package goal.design.system.lrucache;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache {
+
+    int capacity;
+    Map<Integer, Node> map = new HashMap<Integer, Node>();
+    Node head;
+    Node end;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String get(int key) {
+
+        if (map.containsKey(key)) {
+            Node n = map.get(key);
+            if (n.prev != null) {
+                remove(n);
+                setHead(n);
+            }
+            return n.value;
+        }
+        return null;
+    }
+
+    private void setHead(Node n) {
+        if (head != null) {
+            head.prev = n;
+        }
+        n.next = head;
+        n.prev = null;
+        head = n;
+
+        if (end == null) {
+            end = head;
+        }
+    }
+
+    public void remove(Node n) {
+        if (n.next != null) {
+            n.prev.next = n.next;
+            n.next.prev = n.prev;
+
+            n.next = null;
+            n.prev = null;
+        } else {
+            n.prev = null; // have doubt here, for the tail of the node
+        }
+    }
+
+    public void set(int key, String value) {
+        if (map.containsKey(key)) {
+            Node old = map.get(key);
+            old.value = value;
+            if (old.prev != null) {
+                remove(old);
+                setHead(old);
+            }
+        } else {
+            Node newNode = new Node(key, value);
+            if (map.size() >= capacity) {
+                map.remove(end.key);
+                remove(end);
+            }
+            setHead(newNode);
+            map.put(key, newNode);
+        }
+    }
+
+    /*
+     * A doubly linked list node
+     */
+    public static class Node {
+        int key;
+        String value;
+        Node next;
+        Node prev;
+
+        public Node(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+}
---
+++ src/goal/design/system/lrucache/LRUCacheTest.java
@@ -0,0 +1,19 @@
+package goal.design.system.lrucache;
+
+public class LRUCacheTest {
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(4);
+        cache.set(1, "Pankaj Kumar");
+        cache.set(2, "Sushil Kumar");
+        cache.set(3, "Neelam Kumari");
+        cache.set(4, "Arun Kumar");
+
+        System.out.println(cache.get(3));
+        System.out.println(cache.get(1));
+
+        cache.set(5, "Maa"); // issue
+        System.out.println("LRU Cache working!!");
+    }
+
+}
