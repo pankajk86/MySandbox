1a330622359a4318aaefb72c92d7b4d19d03a10b
pankajk86 <kumar.pankaj.it@gmail.com>
2016-07-11 22:15:55 -0500

    phase2: friendship problem

---
+++ src/goal/design/system/friendship/Person.java
@@ -0,0 +1,43 @@
+package goal.design.system.friendship;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Person {
+
+    private long personId;
+    private String firstName;
+    private String lastName;
+    private List<Person> friends;
+
+    public Person(long personId, String firstName, String lastName) {
+        this.personId = personId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.friends = new LinkedList<Person>();
+    }
+
+    public long getPersonId() {
+        return personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public List<Person> getFriends() {
+        return friends;
+    }
+
+    public void addFriends(List<Person> friends) {
+        this.friends.addAll(friends);
+    }
+
+    public void addFriend(Person friend) {
+        this.friends.add(friend);
+    }
+}
