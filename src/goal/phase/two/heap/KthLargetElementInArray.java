fd9d9155b54e96fd018dcef4fecefcbecc98c547
pankajk86 <kumar.pankaj.it@gmail.com>
2016-08-20 22:46:12 -0500

    phase2: KthLargetElementInArray.java

--- src/goal/phase/two/heap/KthLargetElementInArray.java
+++ src/goal/phase/two/heap/KthLargetElementInArray.java
@@ -9,6 +9,34 @@ public class KthLargetElementInArray {
         int a[] = { 4, 3, 1, 7, 5, 2, 18, 9, 10 };
         int k = 3;
 
+        System.out.println("Kth largest in O(n): " + getKthLargest_O_n(a, k));
+        System.out.println("Kth largest in O(k): " + getKthLargest_O_k(a, k));
+    }
+
+    private static int getKthLargest_O_k(int[] a, int k) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k);
+
+        for (int i : a) {
+
+            if (pq.isEmpty()) {
+                pq.add(i);
+            } else {
+                if (pq.size() == k) {
+                    if (pq.peek() < i) {
+                        pq.poll();
+                        pq.add(i);
+                    }
+                } else {
+                    pq.add(i);
+                }
+            }
+        }
+
+        return pq.peek();
+    }
+
+    private static int getKthLargest_O_n(int a[], int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
 
         for (int i : a) {
@@ -23,7 +51,7 @@ public class KthLargetElementInArray {
             pq.poll();
         }
 
-        System.out.println(pq.peek());
+        return pq.peek();
     }
 
 }
